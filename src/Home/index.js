aad7a90ed257d8f4ddb8481e5e70db2ca293321d
Pafs001 <pafs.english@gmail.com>
2022-05-04 21:43:38 -0300

    [CI]:wrench: Adicionar css reset

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
+    <div className='content'>
       <div>
         <h1>Como você imagina o futuro dos pagamentos?</h1>
       </div>
