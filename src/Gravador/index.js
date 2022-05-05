79b6d72f1b67b2570621397461455fb1030a3e10
Pafs001 <pafs.english@gmail.com>
2022-05-05 11:48:48 -0300

    [FIX]:bug: Correção margin content

--- src/Gravador/index.js
+++ src/Gravador/index.js
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 
 export default function Gravador() {
   return (
-    <div className='content primary-bg '>
+    <div className='content green-bg'>
       <div className='row'>
         <div className='recorder-box'>
           <Link to="/depoimento" className='button'>Gravar depoimento</Link>
-          <p>Como serão os pagamentos do futuro? Divida a sua opinião com a gente em até 1 minuto.</p>
+          <div className='box-text'>
+            Como serão os pagamentos do futuro? Divida a sua opinião com a gente em até 1 minuto.
+          </div>        
         </div>
         <div className='recorder-box'>
           <Link to="/agradecemos" className='button'>Gravar palavras-chave</Link>
-          <p>O que você pensa sobre os pagamentos do futuro? Compartilhe as palavras que vem a sua cabeça.</p>
+          <div className='box-text'>
+            O que você pensa sobre os pagamentos do futuro? Compartilhe as palavras que vem a sua cabeça.
+          </div>
         </div>
       </div>
     </div>
