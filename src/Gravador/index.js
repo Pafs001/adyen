a5b56bddec56d119625be34446cbecac117d4a4a
Pafs001 <pafs.english@gmail.com>
2022-05-04 23:41:28 -0300

    [FEAT]:heavy_plus_sign: Adicionar CSS Gravador

--- src/Gravador/index.js
+++ src/Gravador/index.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function Gravador() {
   return (
-    <div className='content'>
-      <div>
-        <div>
-          <Link to="/depoimento">Gravar depoimento</Link>
+    <div className='content primary-bg'>
+      <div className='row'>
+        <div className='recorder-box'>
+          <Link to="/depoimento" className='button'>Gravar depoimento</Link>
           <p>Como serão os pagamentos do futuro? Divida a sua opinião com a gente em até 1 minuto.</p>
         </div>
-        <div>
-          <Link to="/agradecemos">Gravar palavras-chave</Link>
+        <div className='recorder-box'>
+          <Link to="/agradecemos" className='button'>Gravar palavras-chave</Link>
           <p>O que você pensa sobre os pagamentos do futuro? Compartilhe as palavras que vem a sua cabeça.</p>
         </div>
       </div>
